71b8a62baa156d35d9f59dd0e19e6307c8063eb8
IrvinOT <irvintrejoc@gmail.com>
2025-01-23 07:32:20 +0000

    feat(Reloj): Ajuste a Timer
    Cambios para que el reloj se vuelva un timer

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,66 @@
 <template>
+  <div class="time-display">{{ timeInSeconds }}s</div>
   <div class="container" ref="container">
     <div class="clock" ref="clock">
-      <div class="hand hour" :style="{ transform: `rotate(${hour}deg)` }" @mousedown="startDragHand('hour', $event)"></div>
-      <div class="hand minute" :style="{ transform: `rotate(${minute}deg)` }" @mousedown="startDragHand('minute', $event)"></div>
-      <div class="hand second" :style="{ transform: `rotate(${second}deg)` }" @mousedown="startDragHand('second', $event)"></div>
+      <div class="hand second" :style="{ transform: `rotate(${second}deg)` }"
+        @mousedown="startDragHand('second', $event)"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
 const clock = ref(null);
-const hour = ref(0);
-const minute = ref(0);
 const second = ref(0);
-
+let maxMinuet = ref(60)
+let status = ref({
+  draggin: false,
+})
 let draggingHand = null;
 let startAngle = 0;
-let rewindValue = 360/60
+let rewindValue = 360 / maxMinuet.value
+
+const timeInSeconds = computed(() => Math.round((second.value / 360) * maxMinuet.value));
 
 const startDragHand = (hand, event) => {
   draggingHand = hand;
-  const clockRect = clock.value.getBoundingClientRect();
-  const centerX = clockRect.left + clockRect.width / 2;
-  const centerY = clockRect.top + clockRect.height / 2;
-  startAngle = getAngle(centerX, centerY, event.clientX, event.clientY);
+  startAngle = getAngle(event.clientX, event.clientY);
   window.addEventListener('mousemove', dragHand);
   window.addEventListener('mouseup', stopDragHand);
 };
 
-const dragHand = (event) => {
-  if (draggingHand) {
-    const clockRect = clock.value.getBoundingClientRect();
-    const centerX = clockRect.left + clockRect.width / 2;
-    const centerY = clockRect.top + clockRect.height / 2;
-    const currentAngle = getAngle(centerX, centerY, event.clientX, event.clientY);
-    const angleDifference = currentAngle - startAngle;
 
-    switch (draggingHand) {
-      case 'hour':
-        hour.value = (hour.value + angleDifference) % 360;
-        break;
-      case 'minute':
-        minute.value = (minute.value + angleDifference) % 360;
-        break;
-      case 'second':
-        second.value = (second.value + angleDifference) % 360;
-        break;
-    }
-    startAngle = currentAngle;
-  }
+const dragHand = (event) => {
+  if (!draggingHand) return;
+  status.value.draggin = true
+  let currentAngle = getAngle(event.clientX, event.clientY);
+  const angleDifference = currentAngle - startAngle;
+  second.value = normalizeAngle(second.value + angleDifference);
+  startAngle = currentAngle;
 };
 
 const stopDragHand = () => {
+  if (draggingHand) adjustSecondHand()
   draggingHand = null;
+  status.value.draggin = false
   window.removeEventListener('mousemove', dragHand);
   window.removeEventListener('mouseup', stopDragHand);
 };
 
-const getAngle = (cx, cy, ex, ey) => {
-  const dy = ey - cy;
-  const dx = ex - cx;
+const adjustSecondHand = () =>{
+  const secondsFromAngle = Math.round((second.value / 360) * maxMinuet.value);
+  second.value = (secondsFromAngle / maxMinuet.value) * 360; // Mapea el segundo al ángulo correspondiente
+}
+
+const normalizeAngle = (angle) =>  (angle + 360) % 360
+
+const getAngle = (ex, ey) => {
+  let clockRect = clock.value.getBoundingClientRect();
+  let centerX = clockRect.left + clockRect.width / 2;
+  let centerY = clockRect.top + clockRect.height / 2;
+  let dy = ey - centerY;
+  let dx = ex - centerX;
   let theta = Math.atan2(dy, dx); // range (-PI, PI)
   theta *= 180 / Math.PI; // rads to degs, range (-180, 180)
   return theta;
@@ -69,10 +68,9 @@ const getAngle = (cx, cy, ex, ey) => {
 
 onMounted(() => {
   setInterval(() => {
-    const now = new Date();
-    hour.value = (now.getHours() % 12 + now.getMinutes() / 60) * 30;
-    minute.value = now.getMinutes() * rewindValue;
-    second.value = now.getSeconds() * rewindValue;
+    if(status.value.draggin) return
+    let newAngle = second.value - rewindValue
+    second.value = newAngle > 0 ? newAngle : 0
   }, 1000);
 });
 </script>
@@ -100,32 +98,28 @@ onMounted(() => {
   background-color: black;
   transform-origin: 50% 100%;
   left: 50%;
-  cursor: grab; /* Indicate that the hand is draggable */
+  cursor: grab;
+  /* Indicate that the hand is draggable */
 }
 
 .hand:active {
-  cursor: grabbing; /* Indicate that the hand is being dragged */
-}
-
-
-.hour {
-  width: 4px;
-  height: 220px;
-  top: 6%;
-  left: 49.3%;
-}
-
-.minute {
-  width: 3px;
-  height: 235px;
-  top: 3%;
-  left: 49.5%;
+  cursor: grabbing;
+  /* Indicate that the hand is being dragged */
 }
 
 .second {
   width: 2px;
-  height: 250px;
+  height: 240px;
   background-color: red;
-  left: 49.5%;
+  top: 3%;
+  left: 50%;
+}
+
+.time-display {
+  margin-top: 20px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+  color: white;
 }
 </style>
